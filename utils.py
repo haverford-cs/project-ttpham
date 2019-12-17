b577ca262d0cdbf7f37364d242a215783fa2b9b3
Tessa Pham <tessagrethen@gmail.com>
2019-12-17 07:11:28 -0500

    method descriptions

--- utils.py
+++ utils.py
@@ -4,13 +4,32 @@
 import pandas as pd
 
 def load_data():
+    """
+    Load data from given .csv file into train and test sets, split on date condition.
+    
+    Returns:
+        train (DataFrame) -- training set
+        test (DataFrame)  -- test set
+    """
     data = pd.read_csv('stocknews/Combined_News_DJIA.csv')
-    train = data[data['Date'] < '2015-01-01']
-    test = data[data['Date'] > '2014-12-31']
+    train_dates = data['Date'] < '2015-01-01'
+    test_dates = data['Date'] > '2014-12-31'
+    train = data[train_dates]
+    test = data[test_dates]
     return train, test
 
 def preprocess(data):
+    """
+    Preprocess a DataFrame into a list of headlines.
+    
+    Parameters:
+        data (DataFrame) -- a table of dates and top 25 news headlines for each date
+    
+    Returns:
+        headlines (list) -- list of daily news headlines
+                            (25 headlines concatenated for each day)
+    """
     headlines = []
     for row in range(0, len(data.index)):
-        headlines.append(' '.join(str(x) for x in data.iloc[row,2:27]))
+        headlines.append(' '.join(str(x) for x in data.iloc[row, 2:27]))
     return headlines
